37168da1a069b3435ac1c23dcdca155565bdc773
Amedal <2754289818@qq.com>
2024-07-16 14:35:58 +0800

    fix: 更新登陆页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,13 +7,14 @@
       <!-- <SwitchDark class="dark" /> -->
       <div class="login-left font-bold">我的心是旷野的鸟，在你的眼睛里找到了它的天空 - 泰戈尔</div>
       <div class="font-bold title-login">让每一行代码都有改变世界的力量</div>
-
-      <div class="login-form">
-        <div class="login-logo">
-          <!-- <img class="login-icon" src="@/assets/images/logo.png" alt="" /> -->
-          <h2 class="logo-text">BoJack Admin 期待你的加入...</h2>
+      <div class="loginFrom-box">
+        <div class="login-form">
+          <div class="login-logo">
+            <!-- <img class="login-icon" src="@/assets/images/logo.png" alt="" /> -->
+            <h2 class="logo-text">BoJack Admin 期待你的加入...</h2>
+          </div>
+          <LoginForm />
         </div>
-        <LoginForm />
       </div>
     </div>
   </div>
